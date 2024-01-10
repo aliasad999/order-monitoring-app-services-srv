5623ee34e94941ce775d383dc1d979f01cda046a
AsadA <ali.asad@partners.basf.com>
2024-01-10 15:35:17 +0000

    adding mandt  and enabling hint

--- srv/order-monitoring-app-services.js
+++ srv/order-monitoring-app-services.js
@@ -121,11 +121,11 @@ class srvOpenOrders extends cds.ApplicationService {
             // *-------------------------------------------------------------------*
             // End of Code OTC-24554
 
-            if (req.query.SELECT.columns && req.query.SELECT?.columns[0].as === '$count') {
+            if (req.query.SELECT.columns && req.query.SELECT?.columns[0].as === '$count' &&  req.headers?.countcols) {
                 try { 
                     const db = cds.transaction(req);
-                    req.headers.countcols = `SO_MANDT,SO_VBELN,SO_POSNR`;
-                    let query = cds.parse.cql(`SELECT count(*) from ( SELECT DISTINCT ${req.headers.countcols} from  srvOpenOrders_Results   ) WITH HINT(USE_HEX_PLAN,HEX_INDEX_JOIN)` )
+                    req.headers.countcols = `SO_MANDT,${req.headers.countcols}`;
+                    let query = cds.parse.cql(`SELECT count(*) from ( SELECT DISTINCT ${req.headers.countcols} from  srvOpenOrders_Results   ) ` )
                     if (req.query.SELECT.where) query.SELECT.from.SELECT.where = req.query.SELECT.where
                     const distinctCount = (req.query.SELECT.where) ? 
                     await db.run(query)
