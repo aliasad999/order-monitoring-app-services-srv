c424c72155bb2dad03ea76058915bfd2265bee40
AsadA <ali.asad@partners.basf.com>
2024-02-19 16:33:37 +0000

    adding where clause

--- srv/order-monitoring-app-services.js
+++ srv/order-monitoring-app-services.js
@@ -5,6 +5,7 @@ const uuid = require('uuid');
 const status = require('http-status');
 const textBundle = require('./utils/textBundle')
 const log = require("cf-nodejs-logging-support");
+const enableHints = require('./plugins/enable_hints')
 
 class srvOpenOrders extends cds.ApplicationService {
 
@@ -180,7 +181,9 @@ class srvOpenOrders extends cds.ApplicationService {
                 // do nothing
             } else {
                 // cache the query, so that all filter conditions can be consumed.. when any valuehelp is called.
-                const queryString = JSON.stringify(req.query);
+                let query = req.query
+                 query.SELECT.where = req.query.SELECT.where
+                const queryString = JSON.stringify(query);
                 const queryId = `${sessionID}Query`
                 sessionCache.set(queryId, queryString);
             }
