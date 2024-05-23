d765c6e88654ddd69f29b7e5999fc789bbf9646c
AsadA <ali.asad@partners.basf.com>
2024-05-23 11:04:49 +0000

    indent

--- srv/order-monitoring-amo-services.js
+++ srv/order-monitoring-amo-services.js
@@ -18,6 +18,7 @@ class srvOpenOrders extends cds.ApplicationService {
             try {
                 const service = await cds.connect.to('authService');
                 lt_result = await service.get("/authObjectRequest?authObjName=V_VBAK_VKO&sap-client=100");
+                
             } catch (error) {
                 // log.error("[order-monitoring-app-services.js] - Remote service to Cobalt failed ! " + JSON.stringify(error));
                 req.error(413, 'ERROR_AUTH_CALL')
