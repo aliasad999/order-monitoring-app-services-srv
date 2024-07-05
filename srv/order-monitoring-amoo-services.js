e3eb770a1c6984005b58212386375433e58a2578
garcid42 <GARCID42>
2024-07-05 09:49:03 +0000

    Add right destinations

--- srv/order-monitoring-amoo-services.js
+++ srv/order-monitoring-amoo-services.js
@@ -22,7 +22,20 @@ class openOrdersSrv extends cds.ApplicationService {
         // only needed to run this when the server is starting
 
         this.on("submitOrderChangeWF", async req => {
+            var payload = {} // add hardcoded payload here to test
+              
+            try {
+                const bizagiSrv = await cds.connect.to('BizagiService');
+                var bizagiCall = await bizagiSrv.tx(req).send({
+                    method: "POST",
+                    path: "/",
+                    data: payload
+                });
+            } catch (error) {
+                req.error(413, error)
+            }
 
+            return bizagiCall
         });
 
         this.on("submitOrderChange", async req => {
