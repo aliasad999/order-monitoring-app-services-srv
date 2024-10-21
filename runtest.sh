2c0efea04dd41aa901cef0599168ae71b4b2f034
garcid42 <GARCID42>
2024-10-21 14:36:36 +0000

    remove url for plugin from test files

--- runtest.sh
+++ runtest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 npm install --save-dev date-fns
-yes | cf install-plugin https://github.com/SAP/cf-cli-defaultenv-plugin/releases/download/v1.1.1/DefaultEnv.linux64
+yes | cf install-plugin DefaultEnv
 
 if [ $? -eq 0 ]; then
     echo "Plugin installed successfully."
