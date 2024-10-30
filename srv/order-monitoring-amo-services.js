5a830c27b9c71aaeecb8b8004b63531e81afd813
garcid42 <GARCID42>
2024-10-30 16:02:14 +0000

    Add new auth logic with timestamp

--- srv/order-monitoring-amo-services.js
+++ srv/order-monitoring-amo-services.js
@@ -6,6 +6,7 @@ const status = require('http-status');
 const textBundle = require('./utils/textBundle')
 const log = require("cf-nodejs-logging-support");
 const enableHints = require("./plugins/enable_hints");
+const { startOfToday } = require('date-fns');
 
 class srvOpenOrders extends cds.ApplicationService {
 
@@ -14,32 +15,41 @@ class srvOpenOrders extends cds.ApplicationService {
             await cds.run(`SET 'APPLICATION' = 'CAPServices'`);
         })
         this.on("getVBAKAuthObjKeys", async req => {
+            const { VBAKAuthObjectKeys } = await cds.entities ('srvOpenOrders');
+            const todayDate = startOfToday().toISOString().slice(0, 19).replace('T', ' ');
             let lt_result = [];
             let userID = req.user.id;
-            // let authSet = await SELECT.from(VBAKAuthObjectKeys).where ({USERID: userID});
-            try {
-                const service = await cds.connect.to('authService');
-                lt_result = await service.get("/authObjectRequest?authObjName=V_VBAK_VKO&sap-client=100");
-                
-            } catch (error) {
-                // log.error("[order-monitoring-app-services.js] - Remote service to Cobalt failed ! " + JSON.stringify(error));
-                req.error(413, 'ERROR_AUTH_CALL')
+            let vbakAuths = await SELECT.from(VBAKAuthObjectKeys).where`USERID = ${userID}`.limit(1);
+            // Avoid updating authorizations more than once a day
+            // Update only if table empty or outdated
+            if (vbakAuths.length > 0) {
+                if ((vbakAuths[0].LAST_UPDATE === null || vbakAuths[0].LAST_UPDATE < todayDate)) {
+                    updateNeeded = true;
+                }
+            } else {
+                updateNeeded = true;
             }
-                         
-            const { VBAKAuthObjectKeys } = await cds.entities ('srvOpenOrders');
-            await DELETE.from(VBAKAuthObjectKeys).where ({USERID: userID});
-
-            if (lt_result.length !== 0){
-                lt_result.forEach((set) => {
-                    set.USERID = userID;
-                })
-
-                await INSERT.into(VBAKAuthObjectKeys, lt_result);
-            }else{
-                return false;
-            }           
-            return true;
-            
+            if (updateNeeded) {
+                let SQLdate = new Date().toISOString().slice(0, 19).replace('T', ' ');
+                try {
+                    const service = await cds.connect.to('authService');
+                    lt_result = await service.get("/authObjectRequest?authObjName=V_VBAK_VKO&sap-client=100");
+        
+                } catch (error) {
+                    req.error(413, 'ERROR_AUTH_CALL')
+                }
+                await DELETE.from(VBAKAuthObjectKeys).where({ USERID: userID });
+        
+                if (lt_result.length !== 0) {
+                    lt_result.forEach((set) => {
+                        set.LAST_UPDATE = SQLdate;
+                        set.USERID = userID;
+                    })
+                    await INSERT.into(VBAKAuthObjectKeys, lt_result);
+                }
+                return true;
+            }        
+            return false;
         });
 
         /**
