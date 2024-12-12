24252a33d5a60b2e709de054287f2dcd906f049a
David Garcia <garcid42>
2024-12-12 10:20:54 +0000

    comment out POC part

--- srv/order-monitoring-amoo-services.js
+++ srv/order-monitoring-amoo-services.js
@@ -542,11 +542,15 @@ class openOrdersSrv extends cds.ApplicationService {
             let lt_result = []
             // if session id is there, get the cach-ed query and execute it.
             if (sessionCache.get(queryId)) {
-                let NPSTabSelected = req.headers.tabselected;
                 const queryString = sessionCache.get(queryId);
                 const query = JSON.parse(queryString);
                 query.SELECT.from.ref[0] = 'openOrdersSrv.allIssues'
-                serviceHelper.addOrRemoveNPSFilter(query, NPSTabSelected);
+                
+                // POC Refresh only if needed
+                // let NPSTabSelected = req.headers.tabselected;
+                // serviceHelper.addOrRemoveNPSFilter(query, NPSTabSelected);
+                // POC Refresh only if needed
+
                 // make sure pagination is taken into account
                 // if (query.SELECT.limit.rows.val) query.SELECT.limit.rows.val = req.query.SELECT.limit.rows?.val;
                 //query.SELECT.distinct = true;
