279282df15fdc4cf0e774fd0ab8932ad8d46be1a
David Garcia <garcid42>
2025-01-29 17:07:25 +0000

    Add odata call to get incompletion logs

--- srv/order-monitoring-amoo-services.js
+++ srv/order-monitoring-amoo-services.js
@@ -1401,22 +1401,31 @@ class openOrdersSrv extends cds.ApplicationService {
         })
 
         this.on("getIssueReason", async (req) => {
-            let issueReason = []
+            let incompletionLog = []
             let creditData = {}
             let idocData = []
             let atpData = []
             const { salesOrder, salesOrderItem, detailsSalesOrder,
                 DetailsSalesOrderItem, issue, nps, issue_location, material,
                 plant, quantity, uom, dueDate, firstDate } = req.data;
+            let issueLocation = detailsSalesOrder;
+            let issueLocationItem = DetailsSalesOrderItem;
+
             // Call CX only for order incomplete and outbound delivery incomplete (for now)
             if(issue === "01" || issue === "05"){
                 try {
-                    const AMOOUtilsService = await cds.connect.to('AMOOUtilsService');
-                    const query = `/IssueReason(p_mandt='100',p_SalesOrderNumber='${salesOrder}',p_SalesOrderItemNumber='${salesOrderItem}',p_DetailSalesOrderNumber='${detailsSalesOrder}',p_DetailSalesOrderItemNumber='${DetailsSalesOrderItem}',p_IssueId='${issue}',p_NPSId='${nps}',p_issue_location='${issue_location}',p_lang='EN')/Results?sap-client=100`
-                    issueReason = await AMOOUtilsService.tx(req).send({
-                        method: "GET",
-                        path: query
-                    });
+                    // const AMOOUtilsService = await cds.connect.to('AMOOUtilsService');
+                    // const query = `/IssueReason(p_mandt='100',p_SalesOrderNumber='${salesOrder}',p_SalesOrderItemNumber='${salesOrderItem}',p_DetailSalesOrderNumber='${detailsSalesOrder}',p_DetailSalesOrderItemNumber='${DetailsSalesOrderItem}',p_IssueId='${issue}',p_NPSId='${nps}',p_issue_location='${issue_location}',p_lang='EN')/Results?sap-client=100`
+                    // issueReason = await AMOOUtilsService.tx(req).send({
+                    //     method: "GET",
+                    //     path: query
+                    // });
+                    const OMServices = await cds.connect.to('DSLServicesService');
+                    incompletionLog = await OMServices.run(SELECT.from('IncompletionLogsSet').where({
+                        DocumentNumber: issueLocation, // order number in case of 01 and delivery number in case of 05
+                        DocumentItem: issueLocationItem, // order item in case of 01 and delivery item in case of 05
+                        Issue: issue
+                    }))
                 } catch (error) {
                     console.error('Error fetching issue reason:', error);
                 }
@@ -1497,8 +1506,8 @@ class openOrdersSrv extends cds.ApplicationService {
                 }
             }
             const combinedResults = [];
-            issueReason.forEach((item) => {
-                combinedResults.push({ text: item.IssueReason })
+            incompletionLog.forEach((item) => {
+                combinedResults.push({ text: item.IncompletionText })
             })
             for (const prop in creditData) {
                 if (creditData.hasOwnProperty(prop)) {
