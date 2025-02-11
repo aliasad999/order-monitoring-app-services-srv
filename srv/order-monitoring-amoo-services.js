d257ca1c9b0add58ca6672e2b32a075aa5ae2083
David Garcia <garcid42>
2025-02-11 15:20:42 +0000

    add order creation logic again

--- srv/order-monitoring-amoo-services.js
+++ srv/order-monitoring-amoo-services.js
@@ -1018,24 +1018,25 @@ class openOrdersSrv extends cds.ApplicationService {
 
         this.on("READ", "orderCreation", async (req, next) => {
             if (req.query.SELECT.columns && req.query.SELECT?.columns[0].as === '$count' ) {
-                return req.reply({ $count: 0 })
-                // try {
-                //     const db = cds.transaction(req);
-                //     const countCols = "PO_MANDT,PO_EBELN,PO_EBELP"
-                //     let query = cds.parse.cql(`SELECT count(*) from ( SELECT DISTINCT ${countCols} from  openOrdersSrv_orderCreation   ) `)
-                //     if (req.query.SELECT.where) query.SELECT.from.SELECT.where = req.query.SELECT.where
-                //     const distinctCount = (req.query.SELECT.where) ?
-                //         await db.run(query)
-                //         : await db.run(query);
-                //     return req.reply({ $count: Object.values(distinctCount[0])[0] })
-                // } catch (error) {
-                //     log.error("[order-monitoring-app-services.js] - Count query failed ! " + JSON.stringify(error));
-                //     req.error(error)
-                // }
-            }else{
-                return [];
+                // return req.reply({ $count: 0 })
+                try {
+                    const db = cds.transaction(req);
+                    const countCols = "PO_MANDT,PO_EBELN,PO_EBELP"
+                    let query = cds.parse.cql(`SELECT count(*) from ( SELECT DISTINCT ${countCols} from  openOrdersSrv_orderCreation   ) `)
+                    if (req.query.SELECT.where) query.SELECT.from.SELECT.where = req.query.SELECT.where
+                    const distinctCount = (req.query.SELECT.where) ?
+                        await db.run(query)
+                        : await db.run(query);
+                    return req.reply({ $count: Object.values(distinctCount[0])[0] })
+                } catch (error) {
+                    log.error("[order-monitoring-app-services.js] - Count query failed ! " + JSON.stringify(error));
+                    req.error(error)
+                }
             }
-            // await next(req)
+            // else{
+            //     return [];
+            // }
+            await next(req)
         })
 
         this.after("READ", "orderCreation", async (data, req) => {
