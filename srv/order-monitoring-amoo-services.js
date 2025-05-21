f995c750e641a831fa46ab9686aa39c07403b56c
David Garcia <garcid42>
2025-05-21 15:38:36 +0000

    First implementation of ap internal texts logic

--- srv/order-monitoring-amoo-services.js
+++ srv/order-monitoring-amoo-services.js
@@ -48,51 +48,108 @@ class openOrdersSrv extends cds.ApplicationService {
             let SAPTexts = [];
             let salesOrder = req.data.salesOrder;
             let salesOrderItem = req.data.salesOrderItem;
-            let textObjectsData = JSON.parse(req.data.textObjects);
-            // const SAPTextsService = await cds.connect.to('SAPTexts');
+            let orderSystem = req.data.orderSystem;
 
-            const SAPTextsService = await cds.connect.to('DSLServicesService');
+            switch (orderSystem) {
+                case "100": // Cobalt                   
+                    let textObjectsData = [
+                        {
+                            id: "ZAI1",
+                            onItem: false
+                        },
+                        {
+                            id: "ZAV1",
+                            onItem: false
+                        },
+                        {
+                            id: "ZA16",
+                            onItem: true
+                        },
+                        {
+                            id: "ZA10",
+                            onItem: true
+                        },
+                        {
+                            id: "NFL",
+                            onItem: true
+                        }
+                    ]
 
-            for (var i = 0; i < textObjectsData.length; i++) {
-                let textObject = textObjectsData[i];
-                try {
-                    // EXAMPLE
-                    // SAPTexts = await SAPTextsService.get(`/orders/0071396870/items/000020?text_type_id=ZA10&language=EN`);
-                    if (textObject.onItem) {
-                        // SAPTexts = await SAPTextsService.get(`/orders/${salesOrder}/items/${salesOrderItem}?text_type_id=${textObject.id}&language=${textLanguage}`);
-                        SAPTexts = await SAPTextsService.run(SELECT.from('SAPTextsSet').where({
-                            TextId: textObject.id,
-                            TextName: `${salesOrder}${salesOrderItem}`,
-                            TextObject: "VBBP"
-                        }))
-                    } else {
-                        // SAPTexts = await SAPTextsService.get(`/orders/${salesOrder}?text_type_id=${textObject.id}&language=${textLanguage}`);
-                        SAPTexts = await SAPTextsService.run(SELECT.from('SAPTextsSet').where({
-                            TextId: textObject.id,
-                            TextName: `${salesOrder}`,
-                            TextObject: "VBBK"
-                        }))
+                    const SAPTextsService = await cds.connect.to('DSLServicesService');
+
+                    for (var i = 0; i < textObjectsData.length; i++) {
+                        let textObject = textObjectsData[i];
+                        try {
+                            let whereClause = `TextId = '${textObject.id}' and TextName = '${salesOrder}' and TextObject = 'VBBK'`;
+                            if (textObject.onItem) {
+                                whereClause = `TextId = '${textObject.id}' and TextName = '${salesOrder}${salesOrderItem}' and TextObject = 'VBBP'`;
+                            }
+                            SAPTexts = await SAPTextsService.run(SELECT.from('SAPTextsSet').where(whereClause))
+                            if (SAPTexts.length > 0) {
+                                SAPTexts.forEach((text) => {
+                                    SAPTextsEntity.push({
+                                        TextId: text.TextId,
+                                        SAPText: text.Text.replaceAll("--", "\r\n"),
+                                        KeyText: getBundle(req.locale).getText(`SAPText${text.TextId}`),
+                                        TextLanguage: text.TextLang
+                                    })
+                                })
+                            }
+                        } catch (error) {
+                            if (!error.message.includes("No Data Found")) {
+                                req.error(413, error)
+                            }
+                        }
                     }
-                    if (SAPTexts.length > 0) {
-                        SAPTexts.forEach((text) => {
-                            SAPTextsEntity.push({
-                                TextId: text.TextId,
-                                SAPText: text.Text.replaceAll("--", "\r\n"),
-                                KeyText: getBundle(req.locale).getText(`SAPText${text.TextId}`),
-                                TextLanguage: text.TextLang
+                case "200": // EC
+                    break;
+                case "300": // AP
+                    let itemTextIds = ["ZI10"];
+                    let headerTextIds = ["ZH09", "ZH10"]
+                    const APSAPTextsService = await cds.connect.to('DSLServicesService');
+                    let headerIdsFilter = "";
+                    let itemIdsFilter = "";
+
+                    const addTexts = (arrayOfTexts) => {
+                        if (arrayOfTexts.length > 0) {
+                            arrayOfTexts.forEach((text) => {
+                                SAPTextsEntity.push({
+                                    TextId: text.LongTextID,
+                                    SAPText: text.LongText,
+                                    KeyText: getBundle(req.locale).getText(`SAPText${text.LongTextID}`),
+                                    TextLanguage: text.Language
+                                })
                             })
-                        })
+                        }
                     }
-                    // SAPTextsEntity.push({
-                    //     TextId: textObject.id,
-                    //     SAPText: SAPTexts.textLines.join("\r\n"),
-                    //     KeyText: getBundle(req.locale).getText(`SAPText${textObject.id}`)
-                    // })
-                } catch (error) {
-                    if (!error.message.includes("No Data Found")) {
-                        req.error(413, error)
-                    }
-                }
+
+                    headerTextIds.forEach((id)=>{
+                        if(headerIdsFilter){
+                            headerIdsFilter = `LongTextID = '${id}'`
+                        }else{
+                            headerIdsFilter += ` or LongTextID = '${id}'`
+                        }
+                    })
+                    itemTextIds.forEach((id)=>{
+                        if(itemIdsFilter){
+                            itemIdsFilter = `LongTextID = '${id}'`
+                        }else{
+                            itemIdsFilter += ` or LongTextID = '${id}'`
+                        }
+                    })
+
+                    let headerWhereClause = `SalesOrder = '${salesOrder}' and (${headerIdsFilter})`;
+                    let itemWhereClause =  `SalesOrder = '${salesOrder}' and SalesOrderItem = '${salesOrderItem}' and (${itemIdsFilter})`; 
+
+                    // Header
+                    SAPTexts = await APSAPTextsService.run(SELECT.from('A_SalesOrderText').where(headerWhereClause))
+                    addTexts(SAPTexts);
+
+                    // Item
+                    SAPTexts = await APSAPTextsService.run(SELECT.from('A_SalesOrderItemText').where(itemWhereClause))
+                    addTexts(SAPTexts);
+
+                    break;
             }
 
             return SAPTextsEntity;
@@ -1107,11 +1164,11 @@ class openOrdersSrv extends cds.ApplicationService {
             // Deactivated in PROD
             if (process.env.OC_TAB_STATUS === 'ON') {
                 cds
-                .connect("db")
-                .then(({ db }) =>
-                    db?.before("READ", (req) => enableHints(req)
-                    )
-                );
+                    .connect("db")
+                    .then(({ db }) =>
+                        db?.before("READ", (req) => enableHints(req)
+                        )
+                    );
 
                 req.query.SELECT.localized = false;
                 req.query.SELECT.distinct = true;
