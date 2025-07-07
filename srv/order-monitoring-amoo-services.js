b5b8bd50af9beee03a17e9867dc9de794877dff7
David Garcia <garcid42>
2025-07-07 10:04:06 +0000

    Add hints to count query

--- srv/order-monitoring-amoo-services.js
+++ srv/order-monitoring-amoo-services.js
@@ -1346,7 +1346,8 @@ class openOrdersSrv extends cds.ApplicationService {
                             let keyField = fields[0];
                             let subquery = SELECT.distinct(...fields)
                                 .from('openOrdersSrv_orderCreation')
-                                .orderBy(keyField);
+                                .orderBy(keyField)
+                                .hints('USE_HEX_PLAN', 'HEX_INDEX_JOIN');
                             // Add where clause if needed
                             if(query.SELECT.where){
                                 subquery = subquery.where(query.SELECT.where);
