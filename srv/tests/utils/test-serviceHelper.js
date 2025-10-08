9e46a465c0b95014a8076a51cbe666ad230b5ea4
David Garcia <garcid42>
2025-10-08 12:45:33 +0000

    fix test

--- srv/tests/utils/test-serviceHelper.js
+++ srv/tests/utils/test-serviceHelper.js
@@ -13,7 +13,7 @@ describe('Service Helper Test', function () {
 
     it('transformWhereClause - contains with space', function () {
         const originalWhere = `((SO_EDATU_REQUESTED >= ''2025-07-01'' AND SO_EDATU_REQUESTED <= ''2025-07-31'') AND SO_HTEXT LIKE ''%'' || ''Auftrag '' || ''%'' ESCAPE ''^'') AND  SO_IGNORED = 0 AND`;
-        const expectedWhere = `((SO_EDATU_REQUESTED >= '20250701' AND SO_EDATU_REQUESTED <= '20250731') AND SO_HTEXT LIKE '%' || 'Auftrag ' || '%' ESCAPE '^')  AND SO_IGNORED = 0`;
+        const expectedWhere = `((SO_EDATU_REQUESTED >= '20250701' AND SO_EDATU_REQUESTED <= '20250731') AND SO_HTEXT LIKE '%' || 'Auftrag ' || '%' ESCAPE '^') AND SO_IGNORED = 0`;
         const transformedWhere = serviceHelper.transformWhereClause(originalWhere);
         assert.strictEqual(transformedWhere, expectedWhere);
     });
