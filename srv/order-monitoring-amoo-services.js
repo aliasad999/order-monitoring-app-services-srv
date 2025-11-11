75c4f3c7e7d27cc4eab328d0b5168daeaec2a966
AsadA° <asada>
2025-11-11 09:40:14 +0000

    promise.all for async operation

--- srv/order-monitoring-amoo-services.js
+++ srv/order-monitoring-amoo-services.js
@@ -1373,20 +1373,23 @@ class openOrdersSrv extends cds.ApplicationService {
         this.before("CREATE", "FollowupNotes", async (req) => {
             const db = await cds.connect.to('db');
             const entries = req.query.INSERT.entries;
-            for (const entry of entries) {
-                await db.run(
-                    UPDATE('OPENORDERS_DB_ST_FOLLOWUP_NOTES')
-                        .set({ LAST_FOLLOWUPNOTE_FLAG: ' ' })
-                        .where({
-                            MANDT: entry.Client,
-                            VBELN: entry.SalesOrder,
-                            POSNR: entry.OrderItem,
-                            LAST_FOLLOWUPNOTE_FLAG: 'X'
-                        })
-                );
-                // Mark the new entry as last note
-                entry.LastFollowupNoteFlag = 'X';
-            }
+
+            await Promise.all(
+                entries.map(async (entry) => {
+                    await db.run(
+                        UPDATE('OPENORDERS_DB_ST_FOLLOWUP_NOTES')
+                            .set({ LAST_FOLLOWUPNOTE_FLAG: ' ' })
+                            .where({
+                                MANDT: entry.Client,
+                                VBELN: entry.SalesOrder,
+                                POSNR: entry.OrderItem,
+                                LAST_FOLLOWUPNOTE_FLAG: 'X'
+                            })
+                    );
+                    // Mark the new entry as last note
+                    entry.LastFollowupNoteFlag = 'X';
+                })
+            );
         });
         
         // Adding Flag for first entry in the table after deleting one of the FollowupNotes
